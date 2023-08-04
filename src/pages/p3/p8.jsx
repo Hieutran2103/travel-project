ad2f36596356fadcb78eaf4c3ce193b762dcd585
Hieutran2103 <hieut2103@gmail.com>
2023-08-04 22:15:50 +0700

    123214

--- src/pages/p3/p8.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const p8 = () => {
-  return <div>p8</div>;
-};
-
-export default p8;
--- src/pages/welcome/p6.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const p6 = () => {
-  return <div>p6</div>;
-};
-
-export default p6;
